f351aef262d9356e1b047420508f38c13389b968
EasonChen11 <99108285+EasonChen11@users.noreply.github.com>
2022-03-04 17:13:04 +0800

    prime number

--- main.cpp
+++ main.cpp
@@ -1,22 +1,35 @@
 #include <iostream>
 #include <cmath>
+#include <vector>
+#include <set>
 using namespace std;
 int main() {
-    long long int p,i;
+    long long int p,n= pow(10,12);
+    vector<long long int>prime_number_list;
+    set<long long int>Composite_number;
+    for (long long int j = 2; j <= n; ++j) {
+        if(Composite_number.count(j))
+            continue;
+        else
+            prime_number_list.push_back(j);
+        for (long long int k = j*j; k < n; k+=j) {
+            Composite_number.insert(k);
+//            for(auto x:Composite_number)
+//                cout<<x<<endl;
+        }
+    }
+//    for(auto x:prime_number_list)
+//        cout<<x<<endl;
+
     while (cin>>p ){
         if(p==1){
             cout<<"oh no\n";
             continue;
         }
-        long long int k= pow(p,0.5);
-        for(i=2;i<=k;i++){
-            if(p%i==0){
-                cout<<"oh no\n";
-                break;
-            }
-        }
-            if(i>k)
-                cout<<"isprime\n";
+        if(Composite_number.count(p))
+            cout<<"isprime\n";
+        else
+            cout<<"oh no\n";
     }
     return 0;
 }
