8bee98d1d35b76f9eb9211fa8dde2e189067958f
EasonChen11 <99108285+EasonChen11@users.noreply.github.com>
2022-03-04 23:09:07 +0800

    writen again

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 #include <set>
 using namespace std;
 int main() {
-    long long int p,n= pow(10,12);
+    long long int p, i, n= pow(10,3);//根號(max=10^12)
     vector<long long int>prime_number_list;
     set<long long int>Composite_number;
     for (long long int j = 2; j <= n; ++j) {
@@ -12,24 +12,26 @@ int main() {
             continue;
         else
             prime_number_list.push_back(j);
-        for (long long int k = j*j; k < n; k+=j) {
+        for (long long int k = j*j; k <= n; k+=j) {
             Composite_number.insert(k);
-//            for(auto x:Composite_number)
-//                cout<<x<<endl;
         }
     }
-//    for(auto x:prime_number_list)
-//        cout<<x<<endl;
-
+//for(auto x:prime_number_list)
+//    cout<<x<<endl;
     while (cin>>p ){
+        long long int k= pow(p,0.5);
         if(p==1){
             cout<<"oh no\n";
             continue;
         }
-        if(Composite_number.count(p))
+        for (i = 0; prime_number_list[i] <= k; ++i) {
+            if(p%prime_number_list[i]==0){
+                cout<<"oh no\n";
+                break;
+            }
+        }
+        if(prime_number_list[i]>k)
             cout<<"isprime\n";
-        else
-            cout<<"oh no\n";
     }
     return 0;
 }
