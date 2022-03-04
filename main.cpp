240ea293e291ef67c34314af7e6504f395defb51
EasonChen11 <99108285+EasonChen11@users.noreply.github.com>
2022-03-04 15:47:53 +0800

    branch

--- main.cpp
+++ main.cpp
@@ -6,13 +6,20 @@ using namespace std;
 int main() {
     int x,y,p;
     cin>>x>>y>>p;
-    long long k=fp(x,y,p);
-    cout << "this is master!" << std::endl;
+    cout << fp(x,y,p);
     return 0;
 }
 
 long long int fp(int x, int y, int p) {
     int res=1;
-    while ( )
+    while (y!=0 ){
+        if(y%2==1){
+            res*=x;
+            res%=p;
+        }
+        x*=x;
+        x%=p;
+        y/=2;
+    }
     return res;
 }
