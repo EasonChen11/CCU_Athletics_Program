690083c2147b7d8aed4c3113e7eb51829f48433e
EasonChen11 <99108285+EasonChen11@users.noreply.github.com>
2022-03-04 16:42:19 +0800

    prime number

--- main.cpp
+++ main.cpp
@@ -4,6 +4,10 @@ using namespace std;
 int main() {
     long long int p,i;
     while (cin>>p ){
+        if(p==1){
+            cout<<"oh no\n";
+            continue;
+        }
         long long int k= pow(p,0.5);
         for(i=2;i<=k;i++){
             if(p%i==0){
@@ -13,8 +17,6 @@ int main() {
         }
             if(i>k)
                 cout<<"isprime\n";
-
-
     }
     return 0;
 }
