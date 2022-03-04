fe2c2fdae5f3bfea47ffd450062e9af91c86b618
EasonChen11 <99108285+EasonChen11@users.noreply.github.com>
2022-03-04 15:44:08 +0800

    branch

--- main.cpp
+++ main.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
 
+long long int fp(int x, int y, int p);
+
 using namespace std;
 int main() {
+    int x,y,p;
+    cin>>x>>y>>p;
+    long long k=fp(x,y,p);
     cout << "this is master!" << std::endl;
     return 0;
+}
+
+long long int fp(int x, int y, int p) {
+    int res=1;
+    while ( )
+    return res;
 }
