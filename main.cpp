d7dbd3fb4ff59ad7afb781802ad3d61515fc004d
EasonChen11 <99108285+EasonChen11@users.noreply.github.com>
2022-03-11 16:00:29 +0800

    finish

--- main.cpp
+++ main.cpp
@@ -1,19 +1,25 @@
 #include <iostream>
 
-long long int fast_number(long long int a, long long int i);
+long long int fast_number(long long int x, long long int y, long long int n);
 
 using namespace std;
 int main() {
     long long int a,n;
     cin>>a>>n;
-    cout<<fast_number(a,n-2);
+    cout<< fast_number(a, n - 2, n);
     return 0;
 }
 
-long long int fast_number(long long int a, long long int i) {
+long long int fast_number(long long int x, long long int y, long long int p) {
     long long int res=1;
-    while ( ){
-
+    while (y!=0 ){
+        if(y%2==1){
+            res*=x;
+            res%=p;
+        }
+        x*=x;
+        x%=p;
+        y/=2;
     }
     return res;
 }
