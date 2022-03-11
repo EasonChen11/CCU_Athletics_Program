6a4f1a4140439704cf8fcc34951cc229486259d9
EasonChen11 <99108285+EasonChen11@users.noreply.github.com>
2022-03-11 15:57:59 +0800

    first

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+
+long long int fast_number(long long int a, long long int i);
+
 using namespace std;
 int main() {
-    cout <<"this is master";
+    long long int a,n;
+    cin>>a>>n;
+    cout<<fast_number(a,n-2);
     return 0;
+}
+
+long long int fast_number(long long int a, long long int i) {
+    long long int res=1;
+    while ( ){
+
+    }
+    return res;
 }
