aa90b29f91e378199feb27c98b792170927105c0
EasonChen11 <99108285+EasonChen11@users.noreply.github.com>
2022-03-11 23:35:07 +0800

    first

--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 using namespace std;
+int ExGCD(int a,int b,int &x,int &y){
+    if(b==0){
+        x=1;
+        y=0;
+        return a;
+    }
+    int r= ExGCD(b,a%b,x,y);
+    int x1 =x;
+    x=y;
+    y=x1-(a/b*y);
+    return r;
+}
 int main() {
-    cout <<"this is master";
+    int a,b,x,y;
+    a=23;
+    b=9;
+    ExGCD(a,b,x,y);
+    cerr<<"ax+by = "<<a*x+b*y <<endl;
     return 0;
 }
