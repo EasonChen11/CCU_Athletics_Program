306d10e01e93dbdffb59977f02e99e563f183a82
EasonChen11 <99108285+EasonChen11@users.noreply.github.com>
2022-03-12 01:32:22 +0800

    first

--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 using namespace std;
+typedef struct CycleAndTiming{
+    int t,d;
+};
 int ExGCD(int a,int b,int &x,int &y){
     if(b==0){
         x=1;
@@ -13,11 +16,20 @@ int ExGCD(int a,int b,int &x,int &y){
     return r;
 }
 int main() {
-    int a,b,x,y;
-    cin>>a>>b;
-    ExGCD(a,b,x,y);
-    if(x<0)
-        x+=b;
-    cout<<x;
+    int x,y,n,X=0,M=1;
+    cin>>n;
+    CycleAndTiming list[5]={0,0};
+    for (int i = 0; i < n; ++i) {
+        cin>>list[i].t>>list[i].d;
+        M*=list[i].t;
+    }
+    for (int i = 0; i < n; ++i) {
+        ExGCD(list[i].d,list[i].t,x,y);
+        if (x<0)
+            x+=list[i].t;
+        X+=list[i].d*x*(M/list[i].t);
+        cerr<<M/list[i].t<<"*"<<x<<" "<<list[i].t<<endl;
+    }
+    //cout<<X;
     return 0;
 }
