c3880a29e29dbf173f1bfbd999cf52228d3b4b51
EasonChen11 <99108285+EasonChen11@users.noreply.github.com>
2022-03-12 17:26:35 +0800

    finish

--- main.cpp
+++ main.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
+
+bool household_quality(long long int x, long long int y);
+
 using namespace std;
 int main() {
-    cout <<"this is master";
+    long long int x,y;
+    while (cin>>x>>y){
+        if(household_quality(max(x,y), min(x,y)))
+            cout<<"Yes\n";
+        else
+            cout<<"No\n";
+    }
     return 0;
+}
+
+bool household_quality(long long int x, long long int y) {
+    if(x==1 || y==1)
+        return true;
+    else if(x==0 || y==0)
+        return false;
+    return household_quality(y,x%y);
 }
