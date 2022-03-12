90e15238898ce0578f8fc90aab01b306a1378d0e
EasonChen11 <99108285+EasonChen11@users.noreply.github.com>
2022-03-12 17:57:41 +0800

    time_limit

--- main.cpp
+++ main.cpp
@@ -1,25 +1,49 @@
 #include <iostream>
 #include <map>
-
-void prime_number_separate(long long int k, std::pair< int, int > map1);
-
+#include <cmath>
 using namespace std;
+map < long long int, int > prime_number_separate(long long int k);
+
+
+bool prime_number(long long int i);
+
 int main() {
     int n;
     cin>>n;
     while (n--){
         long long int k;
-        map<int,int> prime_number_times;
         cin>>k;
-        prime_number_sperate(k,prime_number_times);
+        map<long long int,int> prime_number_times = prime_number_separate(k);
         for (auto x:prime_number_times)
-            cout<<x.first<<" "<<x.second;
+            cout<<x.first<<" "<<x.second<<" ";
         prime_number_times.clear();
         cout<<endl;
     }
     return 0;
 }
 
-void prime_number_separate(long long int k, map < int, int > map1) {
+map < long long int, int > prime_number_separate(long long int k) {
+    const long long int x=k;
+    map<long long int,int> prime_number_times;
+    prime_number_times.clear();
+    for (long long int i = 2; i <= x; ++i) {
+        if(k==1)
+            break;
+        if(prime_number(i)){
+            while (k%i==0){
+                k/=i;
+                prime_number_times[i]++;
+            }
+        }
+    }
+    return prime_number_times;
+}
 
+bool prime_number(long long int i) {
+    double d= sqrt(i);
+    for (long long int j = 2; j <= d; ++j) {
+        if(i%j==0)
+            return false;
+    }
+    return true;
 }
