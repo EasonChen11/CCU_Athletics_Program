a8af3ed7f29e93b330bab0b8983a5be2fec7e098
EasonChen11 <99108285+EasonChen11@users.noreply.github.com>
2022-03-12 17:39:22 +0800

    first

--- main.cpp
+++ main.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
+#include <map>
+
+void prime_number_separate(long long int k, std::pair< int, int > map1);
+
 using namespace std;
 int main() {
-    cout <<"this is master";
+    int n;
+    cin>>n;
+    while (n--){
+        long long int k;
+        map<int,int> prime_number_times;
+        cin>>k;
+        prime_number_sperate(k,prime_number_times);
+        for (auto x:prime_number_times)
+            cout<<x.first<<" "<<x.second;
+        prime_number_times.clear();
+        cout<<endl;
+    }
     return 0;
+}
+
+void prime_number_separate(long long int k, map < int, int > map1) {
+
 }
