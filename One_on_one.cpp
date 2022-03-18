31226811dc6b5fa1e68eff7aa947f3ee69a99acd
EasonChen11 <99108285+EasonChen11@users.noreply.github.com>
2022-03-18 21:37:03 +0800

    building

--- One_on_one.cpp
+++ One_on_one.cpp
@@ -1,8 +1,7 @@
 #include <iostream>
 #include <algorithm>
-
 using namespace std;
-int win_number(int*,int*,int);
+int win_number(int * enemies, int enemies_number, int * soldiers, int soldiers_number, int n, int win);
 int main(){
     int n;
     while (cin>>n,n ){
@@ -13,12 +12,19 @@ int main(){
         for (int i = 0; i < n; ++i) {
             cin>>soldiers[i];
         }
-        sort(enemies,enemies+n,greater<int>());//升序：sort(begin, end, less<Type>());
-        sort(soldiers,soldiers+n,greater<int>());//降序：sort(begin, end, greater<Type>());
-        int k;
-        cout<<win_number(enemies,soldiers,n)<<endl;
+        sort(enemies,enemies+n,less<int>());//升序：sort(begin, end, less<Type>());
+        sort(soldiers,soldiers+n,less<int>());//降序：sort(begin, end, greater<Type>());
+        cout << win_number(enemies, 0, soldiers, 0, n, 0) << endl;
     }
 }
-int win_number(int* enemies,int* soldiers,int n){
-
+int win_number(int * enemies, int enemies_number, int * soldiers, int soldiers_number, int n, int win) {
+    if(enemies_number>=n || soldiers_number>=n)
+        return win;
+    if(enemies[enemies_number]<soldiers[soldiers_number]){
+       return win_number(enemies,enemies_number+1,soldiers,soldiers_number+1,n,win+1);
+    } else if(enemies[enemies_number]>soldiers[soldiers_number]){
+        return win_number(enemies,enemies_number,soldiers,soldiers_number+1,n,win);
+    } else{
+        return win_number(enemies,enemies_number+1,soldiers,soldiers_number+1,n,win);
+    }
 }
