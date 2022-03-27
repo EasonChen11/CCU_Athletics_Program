a6b78969dfa23b4476fc806aeb07267d9303f20e
EasonChen11 <99108285+EasonChen11@users.noreply.github.com>
2022-03-28 01:28:07 +0800

    greedy2_B time limit

--- Scheduling_on_many_machines.cpp
+++ Scheduling_on_many_machines.cpp
@@ -1,17 +1,17 @@
 #include <iostream>
 #include <algorithm>
-#include <queue>
-#include <cstdlib>
+//#include <queue>
+//#include <cstdlib>
 using namespace std;
 //想法:
 // 把一個任務放入機器(先讓機器任務時數=0)，
 // 在放入下一個任務進入目前總和時間最小的機器
 // 排序機器總和時間
 //重複以上，最後輸出最常機器的時間
-int compare(void const* a,void const *b){
+/*int compare(void const* a,void const *b){
     return *(long long int *)b<*(long long int *)a;
 }
-
+*/
 int main(){
     int T;
     cin>>T;
@@ -19,14 +19,15 @@ int main(){
         int Case_number,machines_number;
         cin>>Case_number>>machines_number;
         long long int Case_time;
-        long long int *machines_time = (long long int*)calloc(machines_number,sizeof (long long int));
+        //long long int *machines_time = (long long int*)calloc(machines_number,sizeof (long long int));
+        long long int machines_time[100000]={0};
         for (int i = 0; i < Case_number; ++i) {
             cin>>Case_time;
             machines_time[0]+=Case_time;
             //qsort(machines_time,machines_number, sizeof(long long int ),compare);
-            sort(machines_time,machines_time+machines_number,less<long long int>());
+            sort(machines_time,machines_time+machines_number);
         }
         cout<<machines_time[machines_number-1]<<endl;
-        free(machines_time);
+        //free(machines_time);
     }
 }
