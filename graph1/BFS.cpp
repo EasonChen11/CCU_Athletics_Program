cebc1a4ae2f78233ed3016a4d44d1516422e73e6
EasonChen11 <99108285+EasonChen11@users.noreply.github.com>
2022-04-08 22:32:36 +0800

    BFS.cpp finish

--- graph1/BFS.cpp
+++ graph1/BFS.cpp
@@ -1,3 +1,49 @@
-//
-// Created by ysche on 2022/4/8.
-//
+#include <iostream>
+#include <queue>
+#include <vector>
+using namespace std;
+int main()
+{
+    int row, col;
+    cin >> row >> col;
+    vector<vector<char>> graph(row, vector<char>(col));
+    queue<pair<int, int>> q;
+    for (int i = 0; i < row; i++)
+        for (int j = 0; j < col; j++)
+        {
+            cin >> graph[i][j];
+            if (graph[i][j] == 'A')
+                q.push({i, j});
+        }
+    int dir[5] = {0, 1, 0, -1, 0};//(0,1),(1,0),(0,-1),(-1,0)
+    int ans = 0;
+    while (!q.empty())
+    {
+        unsigned long long int size = q.size();
+        while (size--)
+        {
+            int x = q.front().first;
+            int y = q.front().second;
+            q.pop();//在q點
+            if(graph[x][y]=='#')
+                continue;
+            if (graph[x][y] == 'B')
+            {
+                cout << "YES\n";
+                cout << ans << endl;
+                return 0;
+            }
+            graph[x][y] = '#';
+            for (int k = 0; k < 4; k++)
+            {
+                int i = x + dir[k];
+                int j = y + dir[k + 1];
+                if (i >= 0 && i < row && j >= 0 && j < col && graph[i][j] != '#')
+                    q.push({i, j});
+            }
+        }
+        ans++;
+    }
+    cout << "NO\n";
+    return 0;
+}
