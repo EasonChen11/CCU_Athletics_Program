fea34e95ef91a3ad43838ac149a7d53707193b64
EasonChen11 <99108285+EasonChen11@users.noreply.github.com>
2022-04-09 01:01:54 +0800

    Connected_Components more solution

---
+++ graph1/Connected_Components_BFS.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include<algorithm>
+#include <vector>
+#include <queue>
+using namespace std;
+int main() {
+    int nodes,edge;
+    cin>>nodes>>edge;
+    vector<vector<int>> root(nodes);
+    vector<bool> visit(nodes, false);
+    vector<int> points(nodes);
+    for(int &x:points)
+        cin>>x;
+    for (int i = 0; i < edge; ++i) {
+        pair<int,int> link;
+        cin>>link.first>>link.second;
+        root[link.first-1].push_back(link.second-1);
+        root[link.second-1].push_back(link.first-1);
+    }
+    vector<long long int> ans(nodes,0);
+    queue<int> st;
+    for (int i = 0; i < nodes; ++i) {
+        if(visit[i])
+            continue;
+        st.push(i);
+        while (!st.empty() ){
+            int node=st.front();
+            st.pop();
+            if(visit[node])
+                continue;
+            visit[node]=true;
+            ans[i]+=points[node];
+            for(auto x:root[node]){
+                if(visit[x])
+                    continue;
+                st.push(x);
+            }
+        }
+    }
+    sort(ans.begin(),ans.end());
+    for(auto x:ans){
+        if(x)
+            cout<<x<<" ";
+    }
+    return 0;
+}
