49df2a879a9b3bc0975f347a01b8f937c1797d02
EasonChen11 <99108285+EasonChen11@users.noreply.github.com>
2022-04-22 16:26:24 +0800

    Build-airport.cpp finish

---
+++ graph3/Build-airport.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Union_find{
+public:
+    int parent[1000+5],Size[1000+5];
+    void init(){
+        for (int i = 1; i <= 1000+5; ++i) {
+            parent[i]=i;
+            Size[i]=1;
+        }
+    }
+    int Finds(int x){
+        if(parent[x]==x)
+            return x;
+        return parent[x]= Finds(parent[x]);
+    }
+    void Union(int a,int b){
+        int x= Finds(a);
+        int y= Finds(b);
+        if(x==y)
+            return;
+        if(Size[x]>Size[y])
+            swap(x,y);
+        parent[x]=parent[y];
+        Size[y]+=Size[x];
+    }
+};
+int main() {
+    int node,edge;
+    cin>>node>>edge;
+    Union_find solution;
+    solution.init();
+    for (int i = 0; i < edge; ++i) {
+        int a,b;
+        cin>>a>>b;
+        if(solution.Finds(a)==solution.Finds(b))continue;
+        solution.Union(a,b);
+    }
+    int ans=0;
+    for (int z=1;z<=node;z++) {
+        if(solution.parent[z]==z) ans++;
+    }
+    if(ans<=1)
+        cout<<0<<endl;
+    else
+        cout<<ans<<endl;
+    return 0;
+}
