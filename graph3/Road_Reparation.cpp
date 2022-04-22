a6c117f68d4bf3207c77b1e3fef874039a0e9e53
EasonChen11 <99108285+EasonChen11@users.noreply.github.com>
2022-04-23 01:05:51 +0800

    Tree_Center.cpp build

--- graph3/Road_Reparation.cpp
+++ graph3/Road_Reparation.cpp
@@ -13,9 +13,9 @@ int main(){
         city_to_city[first].push_back(input);
         city_to_city[input.second].push_back({input.first,first});
     }
-    vector<bool> visit(total_city+1, false);
+    vector<bool> visit(total_city+1, false);//檢查走訪
     long long int ans_weight=0;
-    priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
+    priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;//每次都選最小邊
     pq.push({0,1});
     while (!pq.empty()){
         int now_point=pq.top().second,weight=pq.top().first;
@@ -24,14 +24,14 @@ int main(){
             continue;
         visit[now_point]=true;
         ans_weight+=weight;
-        for(auto x:city_to_city[now_point]){
+        for(auto x:city_to_city[now_point]){//推入連結的每個節點
             if(visit[x.second])
                 continue;
             pq.push(x);
         }
     }
     for(int i=1;i<=total_city;i++){
-        if(!visit[i]){
+        if(!visit[i]){//有點沒走過
             cout<<"IMPOSSIBLE"<<endl;
             return 0;
         }
